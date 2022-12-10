2b031c85342d870147ee9274eee3f4aced1ed455
Simeodey <adamand12@abv.bg>
2022-12-10 14:07:55 +0200

    ttt

--- src/js/Notification.js
+++ src/js/Notification.js
@@ -1,3 +1,7 @@
+import classNames from "classnames";
+import { formatCurrency } from "./utils";
+import Card from "./Card";
+
 export default class Notification {
   static get types() {
     return {
@@ -12,14 +16,19 @@ export default class Notification {
     this.container.classList.add("notification-container");
   }
 
-  render() {
+  render({type,price}) {
     const template = `
-<div class="notification type-pepperoni">
+<div class="notification type-${type} ${classNames({"is-danger": type === Card.types.HAWAIIAN,})}">
   <button class="delete"></button>
-  🍕 <span class="type">pepperoni</span> (<span class="price">0,00</span>) has been added to your order.
+  🍕 <span class="type">${type}</span> (<span class="price">${formatCurrency(price)}</span>) has been added to your order.
 </div>
     `;
-
     this.container.innerHTML = template;
+   // this.container.querySelector(".delete").addEventListener("click",()=>{console.log("Delete button clicked")});
+    document.querySelector(".notifications").appendChild(this.container);
+    this.container.querySelector(".delete").addEventListener("click",()=>{console.log("Delete button clicked"); this.empty();});
+  }
+  empty(){
+    this.container.innerHTML = "";
   }
 }
